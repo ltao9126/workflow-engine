ed2ba691af6b0f8ff16165462fd15d091495b4c0
liu.tao <liutao9126@163.com>
2019-07-24 15:05:40 +0800

    remove ui module

--- workflow-uiservice/src/main/java/com/greatech/workflow/uiservice/api/UINodeDeal.java
+++
@@ -1,9 +0,0 @@
-package com.greatech.workflow.uiservice.api;
-
-/**
- * Created by ltao on 17-6-28.
- * 对外公开的接口
- */
-public interface UINodeDeal {
-
-}
--- workflow-uiservice/src/main/webapp/index.jsp
+++
@@ -1,6 +0,0 @@
-<html>
-<body>
-<h2>Hello World!</h2>
-<a href="index.html">flow</a>
-</body>
-</html>
